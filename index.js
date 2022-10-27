57dfc002923de3016d8219419c4361a015c7060a
Tawhidul3233 <mdtawhidulislam3233@gmail.comm>
2022-10-27 13:52:46 +0600

    api data loaded success

--- index.js
+++ index.js
@@ -3,3 +3,15 @@ const app = express();
 const port = process.env.PORT || 5000 
 
 const courses = require('./Data/FakeData.json')
+
+
+app.get('/', (req , res)=>{
+     res.send(courses)
+})
+
+
+
+
+app.listen(port, () => {
+     console.log(` skillbd ${port}`)
+   })
