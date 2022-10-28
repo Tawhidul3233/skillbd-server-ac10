a5ebbfa662cc1a35257bad8780defd8b413c8ec7
Tawhidul3233 <mdtawhidulislam3233@gmail.comm>
2022-10-28 21:30:07 +0600

    new api added

--- index.js
+++ index.js
@@ -1,31 +1,43 @@
 const express = require('express')
 const app = express();
 const cors = require('cors')
-const port = process.env.PORT || 5000 
+const port = process.env.PORT || 5000
 
-const courses = require('./Data/FakeData.json')
+const courses = require('./Data/FakeData.json');
+const categories = require('./Data/Categories.json')
+
 
 app.use(cors())
 
 
-app.get('/', (req , res)=>{
+app.get('/', (req, res) => {
      res.send(courses)
 })
 
-
-app.get('/courses/:id', (req, res)=>{
+app.get('/courses/:id', (req, res) => {
      const id = req.params.id
-     const course = courses.find(c => c.id === id)
-     res.send(course)
-});
+     const courseItems = courses.find(c => c.id === id)
+     res.send(courseItems);
+})
 
-app.get('/cetagories/:id', (req, res)=> {
+app.get('/checkout/:id', (req, res) => {
      const id = req.params.id
-     const category = courses.filter(c => c.category_id === id)
-     res.send(category)
+     const courseItems = courses.find(c => c.id === id)
+     res.send(courseItems);
+})
+
+
+app.get('/categories', (req, res) => {
+     res.send(categories);
+})
+
+app.get('/categories/:id', (req, res) => {
+     const id = req.params.id
+     const categoryItems = courses.filter(c => c.category === id)
+     res.send(categoryItems);
 })
 
 
 app.listen(port, () => {
      console.log(` skillbd ${port}`)
-   })
+})
